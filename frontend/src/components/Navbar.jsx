1b18f485dc51426906f3d625b3ae93b4bc759dd2
ice1187 <icing1187@gmail.com>
2025-04-03 00:11:01 +0800

    fix: make navbar UI a little better(?). I'm not a UI guy, so it may actually be worse. Whatever.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,31 @@
 function Navbar({ currentPage, setCurrentPage }) {
   return (
-    <nav className="bg-primary p-4 shadow-md">
-      <div className="container mx-full flex flex-row justify-start items-center space-x-4">
-        <h1 className="text-secondary-dark text-2xl font-bold">好味腦波弱百科</h1>
-        <button
-          className={`px-3 py-2 rounded text-secondary-dark ${
-            currentPage === 'page1'
-              ? 'bg-primary font-bold'
-              : 'hover:bg-secondary-dark hover:text-primary-light'
-          }`}
-          onClick={() => setCurrentPage('page1')}
-        >
-          哼猜學堂
-        </button>
+    <nav className="bg-primary w-full p-2 shadow-md">
+      <div className="max-w-7xl mx-auto">
+        <div className="container flex flex-row justify-start items-center space-x-4">
+          <h1 className="text-secondary-dark text-2xl font-bold">好味腦波弱百科</h1>
+          <button
+            className={`px-3 py-2 rounded text-secondary-dark ${
+              currentPage === 'page1'
+                ? 'bg-primary font-bold'
+                : 'hover:bg-secondary-dark hover:text-primary-light'
+            }`}
+            onClick={() => setCurrentPage('page1')}
+          >
+            哼猜學堂
+          </button>
 
-        <button
-          className={`px-3 py-2 rounded text-secondary-dark ${
-            currentPage === 'page2'
-              ? 'bg-primary font-bold'
-              : 'hover:bg-secondary-dark hover:text-primary-light'
-          }`}
-          onClick={() => setCurrentPage('page2')}
-        >
-          Podcast Timecode
-        </button>
+          <button
+            className={`px-3 py-2 rounded text-secondary-dark ${
+              currentPage === 'page2'
+                ? 'bg-primary font-bold'
+                : 'hover:bg-secondary-dark hover:text-primary-light'
+            }`}
+            onClick={() => setCurrentPage('page2')}
+          >
+            Podcast Timecode
+          </button>
+        </div>
       </div>
     </nav>
   )
